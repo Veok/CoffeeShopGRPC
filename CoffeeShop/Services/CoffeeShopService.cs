5ef37f57a81b3e4c2855dac68b405ee20e8bb1d8
pawel.lelental <pawel.lelental@ihsmarkit.com>
2021-05-26 10:47:54 +0200

    Initial commit

---
+++ CoffeeShop/Services/CoffeeShopService.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace CoffeeShop.Services
+{
+    public class CoffeeShopService : CoffeeShop.CoffeeShopBase
+    {
+        private readonly ILogger<CoffeeShopService> _logger;
+
+        public CoffeeShopService(ILogger<CoffeeShopService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CoffeeResponse> OrderCoffee(OrderRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("Received order with coffee: {Coffee}", request.Coffee);
+            var isCoffeeInMenu = CoffeeMenu().TryGetValue(request.Coffee, out var price);
+
+            if (isCoffeeInMenu)
+            {
+                return Task.FromResult(new CoffeeResponse
+                {
+                    Coffee = request.Coffee,
+                    Receipt = $"{request.Coffee}: {price}$"
+                });
+            }
+
+            const string message = "Invalid Coffee: {Coffee}";
+            _logger.LogError(message, request.Coffee);
+            throw new ArgumentOutOfRangeException(nameof(request.Coffee), request.Coffee, message);
+        }
+
+        private static Dictionary<Coffee, int> CoffeeMenu()
+        {
+            return new()
+            {
+                {Coffee.Americano, 11},
+                {Coffee.Espresso, 8},
+                {Coffee.Latte, 14},
+                {Coffee.FlatWhite, 14}
+            };
+        }
+    }
+}
